5f1deb0c03b3b96c26541b5d319688aaa62f4c10
imthiazSB <imtiaz@softbuilders.ae>
2025-11-13 18:00:55 +0400

    feat(config): add @nestjs/config for environment management
    Add @nestjs/config package to handle environment variables globally across the application. This enables centralized configuration management and easier access to environment-specific settings.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,20 @@ import { AuthModule } from './auth/auth.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { sequelize } from './database/sequelize.config';
 import { NotificationModule } from './notification/notification.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 
 @Module({
   imports: [
+    // Load environment variables
+    ConfigModule.forRoot({
+      isGlobal: true, // make it available in all modules
+    }),
     SequelizeModule.forRoot({ ...sequelize.options }),
     AuthModule,
     NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
